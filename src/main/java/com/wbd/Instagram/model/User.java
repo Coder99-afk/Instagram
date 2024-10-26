f00111d80c5f908854986b32b64902da47e85cbb
Coder99-afk <ayush.swain@wbd.com>
2024-10-26 17:42:54 +0530

    4th commit

--- src/main/java/com/wbd/Instagram/model/User.java
+++ src/main/java/com/wbd/Instagram/model/User.java
@@ -1,14 +1,12 @@
 package com.wbd.Instagram.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
+@Table(name = "users")//Table name in postgresql
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
